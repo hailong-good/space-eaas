bcad714a4ebc7f5e67840ab380f3f3acd5f62ed2
海龙 <2297040028@qq.com>
2024-01-07 19:29:01 +0800

    feat: 注册接口

--- app/controller/login.js
+++ app/controller/login.js
@@ -1,12 +1,40 @@
-const Controller = require('egg').Controller;
+const Controller = require("egg").Controller;
 
 class HomeController extends Controller {
-  async index() {
+  async register() {
     const { ctx, app } = this;
     this.db;
-    this.ctx.body = 'Hello world';
-    // this.ctx.body = ctx.response
-    console.log('\n\n\n\n\n\n\n\n',await app.mysql.query('SELECT * FROM users;'));
+    // 获取参数
+    const reqParam = ctx.request.body || {};
+    // 检查数据库有没有存在重复的用户
+    const selectUser =
+      (await app.mysql.query(
+        `SELECT * FROM users WHERE username LIKE '%${reqParam.userName}%';`
+      )) || [];
+    // 没有存在相同用户
+    if (selectUser.length === 0) {
+      app.mysql.query(
+        `INSERT INTO users (id,username, password) VALUES (999,'${reqParam.userName}', '${reqParam.password}');`
+      );
+      console.log(
+        "\n\n\n\n\n\n\n\n",
+        await app.mysql.query("SELECT * FROM users;")
+      );
+      return (ctx.body = { code: 200, msg: "注册成功" });
+    } else {
+      // 有相同用户存在
+       (ctx.body = { code: 500, msg: "用户名已存在" });
+    }
+    console.log(
+        "\n\n\n\n\n\n\n\n",
+        await app.mysql.query("SELECT * FROM users;")
+      );
+    // console.log(
+    //   "\n\n\n\n\n\n\n",
+    //   ctx.request.body,
+    //   "\n\n\n\n\n\n\n",
+    //   selectUser.length
+    // );
   }
 }
 
--- config/config.default.js
+++ config/config.default.js
@@ -1,29 +1,4 @@
-exports.keys = 'xuhailong';
-
-// exports.sequelize = {
-//     dialect: 'mysql', // 表示使用mysql数据库
-//     host: '127.0.0.1', // 连接的数据库主机地址
-//     port: 3306,        // 数据库端口号
-//     database: 'space-eaas', // 数据库名
-//     username: 'root', // 数据库用户名
-//     password: 'xu2297040028', // 数据库密码
-//     timezone: '+8:00', // 由于orm用的UTC时间，这里必须加上东八区，否则取出来的时间相差8小时
-//     define: {  // model的全局配置
-//       timestamps: false,   // 添加create,update,delete时间戳
-//       freezeTableName: false,  // 防止修改表名为复数
-//       underscored: false  // 防止驼峰式字段被默认转为下划线
-//     },
-//     app: true,
-//     dialectOptions: { // 让读取date类型数据时返回字符串而不是UTC时间
-//       dateStrings: true,
-//       typeCast(field, next) {
-//         if(field.type === "DATETIME"){
-//           return field.string();
-//         }
-//         return next();
-//       }
-//     }
-//   }
+exports.keys = "xuhailong";
 
 exports.mysql = {
   client: {
@@ -35,4 +10,15 @@ exports.mysql = {
   },
   app: true,
   agent: false,
+};
+// 解决csrf报错问题
+exports.security = {
+  csrf: {
+    enable: false,
+  },
+};
+// 解决跨域
+exports.cors = {
+  origin: "*",
+  allowMethods: "GET,HEAD,PUT,POST,DELETE,PATCH",
 };
