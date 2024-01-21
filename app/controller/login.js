89a2069a0216c59076493e52a8011be5b4593a7a
海龙 <2297040028@qq.com>
2024-01-21 18:04:52 +0800

    feat: 登陆和验证token接口

--- app/controller/login.js
+++ app/controller/login.js
@@ -1,9 +1,10 @@
 const Controller = require("egg").Controller;
+const jwt = require("jsonwebtoken");
 
 class HomeController extends Controller {
+  // 注册
   async register() {
     const { ctx, app } = this;
-    this.db;
     // 获取参数
     const reqParam = ctx.request.body || {};
     // 检查数据库有没有存在重复的用户
@@ -23,18 +24,51 @@ class HomeController extends Controller {
       return (ctx.body = { code: 200, msg: "注册成功" });
     } else {
       // 有相同用户存在
-       (ctx.body = { code: 500, msg: "用户名已存在" });
+      ctx.body = { code: 500, msg: "用户名已存在" };
     }
     console.log(
-        "\n\n\n\n\n\n\n\n",
-        await app.mysql.query("SELECT * FROM users;")
-      );
+      "\n\n\n\n\n\n\n\n",
+      await app.mysql.query("SELECT * FROM users;")
+    );
     // console.log(
     //   "\n\n\n\n\n\n\n",
     //   ctx.request.body,
     //   "\n\n\n\n\n\n\n",
     //   selectUser.length
     // );
+  }
+  // 登陆
+  async login() {
+    const { ctx, app } = this;
+    const { username, password } = ctx.request.body || {};
+    if (!username || !password)
+      return (ctx.body = { code: 500, msg: "请输入用户名密码" });
+    const payload = {
+      userName: username,
+      passWord: password,
+    };
+
+    const secretKey = "yourSecretKey";
+    const token = jwt.sign(payload, secretKey, { expiresIn: "1h" });
+    ctx.body = {
+      code: 200,
+      msg: "登陆成功",
+      token,
+    };
+  }
+  // 验证token
+  async verifyToken() {
+    const { ctx, app } = this;
+    const { token } = ctx.query;
+
+    try {
+      const decoded = jwt.verify(token, "yourSecretKey");
+
+      console.log("\n\n\n\n", decoded);
+      return (ctx.body = decoded);
+    } catch (err) {
+      return (ctx.body = { status: false, msg: "验证失败" });
+    }
   }
 }
 
--- app/router.js
+++ app/router.js
@@ -1,5 +1,7 @@
 module.exports = (app) => {
     const { router, controller } = app;
     router.get('/', controller.home.index);
-    router.post('/register', controller.login.register)
+    router.post('/register', controller.login.register);
+    router.post('/login', controller.login.login);
+    router.get('/verifyToken', controller.login.verifyToken);
 };
